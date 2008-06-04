124bdd37cb00f8b57728caa1de3d7be0c20ab504
Ryan Nichol <r.nichol@ucl.ac.uk>
2008-06-04 13:15:03 +0000

    Updated to new header and event dataformat

--- RawAnitaHeader.cxx
+++ RawAnitaHeader.cxx
@@ -29,13 +29,14 @@ RawAnitaHeader::RawAnitaHeader(AnitaEventHeader_t *hdPtr, Int_t trun, UInt_t tre
    payloadTime=hdPtr->unixTime;
    payloadTimeUs=hdPtr->unixTimeUs;
    gpsSubTime=hdPtr->gpsSubTime;
+   turfEventId=hdPtr->turfEventId;
    eventNumber=hdPtr->eventNumber;
    calibStatus=hdPtr->calibStatus;
    priority=hdPtr->priority;
    turfUpperWord=hdPtr->turfUpperWord;
    otherFlag=hdPtr->otherFlag;
    errorFlag=hdPtr->errorFlag;
-   otherFlag3=hdPtr->otherFlag3;
+   surfSlipFlag=hdPtr->surfSlipFlag;
    nadirAntTrigMask=hdPtr->nadirAntTrigMask;
    antTrigMask=hdPtr->antTrigMask;
    trigType=hdPtr->turfio.trigType;
@@ -46,11 +47,15 @@ RawAnitaHeader::RawAnitaHeader(AnitaEventHeader_t *hdPtr, Int_t trun, UInt_t tre
    ppsNum=hdPtr->turfio.ppsNum;
    deadTime=hdPtr->turfio.deadTime;
    bufferDepth=hdPtr->turfio.bufferDepth;
+   reserved=hdPtr->turfio.reserved;
    upperL1TrigPattern=hdPtr->turfio.upperL1TrigPattern;
    lowerL1TrigPattern=hdPtr->turfio.lowerL1TrigPattern;
    upperL2TrigPattern=hdPtr->turfio.upperL2TrigPattern;
    lowerL2TrigPattern=hdPtr->turfio.lowerL2TrigPattern;
    l3TrigPattern=hdPtr->turfio.l3TrigPattern;
+   memcpy(otherTrigPattern,hdPtr->turfio.otherTrigPattern,3*sizeof(UShort_t));
+   nadirL1TrigPattern=hdPtr->turfio.nadirL1TrigPattern;
+   nadirL2TrigPattern=hdPtr->turfio.nadirL2TrigPattern;
    run=trun;
    realTime=trealTime;
    triggerTime=ttriggerTime;
@@ -58,68 +63,6 @@ RawAnitaHeader::RawAnitaHeader(AnitaEventHeader_t *hdPtr, Int_t trun, UInt_t tre
    goodTimeFlag=tgoodTimeFlag;
 }
 
-RawAnitaHeader::RawAnitaHeader(Int_t    trun,
-			       UInt_t   trealTime,
-			       UInt_t   tpayloadTime,
-			       UInt_t   tpayloadTimeUs,
-			       UInt_t   tgpsSubTime,
-			       UInt_t   teventNumber,
-			       UShort_t tcalibStatus,
-			       UChar_t  tpriority,
-			       UChar_t  tturfUpperWord,
-			       UChar_t  totherFlag,
-			       UChar_t  terrorFlag,
-			       UChar_t  totherFlag3,
-			       UChar_t  tnadirAntMask,
-			       UInt_t   tantTrigMask,
-			       UChar_t  ttrigType,
-			       UChar_t  tl3Type1Count,
-			       UShort_t ttrigNum,
-			       UInt_t   ttrigTime,
-			       UInt_t   tc3poNum,
-			       UShort_t tppsNum,
-			       UShort_t tdeadTime,
-			       UChar_t  tbufferDepth,
-			       UShort_t tupperL1TrigPattern,
-			       UShort_t tlowerL1TrigPattern,
-			       UShort_t tupperL2TrigPattern,
-			       UShort_t tlowerL2TrigPattern,
-			       UShort_t tl3TrigPattern,
-			       UInt_t   ttriggerTime,
-			       UInt_t   ttriggerTimeNs,
-			       Int_t    tgoodTimeFlag)
-{
-   payloadTime=tpayloadTime;
-   payloadTimeUs=tpayloadTimeUs;
-   gpsSubTime=tgpsSubTime;
-   eventNumber=teventNumber;
-   calibStatus=tcalibStatus;
-   priority=tpriority;
-   turfUpperWord=tturfUpperWord;
-   otherFlag=totherFlag;
-   errorFlag=terrorFlag;
-   otherFlag3=totherFlag3;
-   nadirAntTrigMask=tnadirAntMask;
-   antTrigMask=tantTrigMask;
-   trigType=ttrigType;
-   l3Type1Count=tl3Type1Count;
-   trigNum=ttrigNum;
-   trigTime=ttrigTime;
-   c3poNum=tc3poNum;
-   ppsNum=tppsNum;
-   deadTime=tdeadTime;
-   bufferDepth=tbufferDepth;
-   upperL1TrigPattern=tupperL1TrigPattern;
-   lowerL1TrigPattern=tlowerL1TrigPattern;
-   upperL2TrigPattern=tupperL2TrigPattern;
-   lowerL2TrigPattern=tlowerL2TrigPattern;
-   l3TrigPattern=tl3TrigPattern;
-   run=trun;
-   realTime=trealTime;
-   triggerTime=ttriggerTime;
-   triggerTimeNs=ttriggerTimeNs;
-   goodTimeFlag=tgoodTimeFlag;
-} 
 
 char *RawAnitaHeader::trigTypeAsString()
 {
