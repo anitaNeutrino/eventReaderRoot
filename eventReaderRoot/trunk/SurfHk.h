14dc6e3b4d5b72b891eb0f60c5e195d08ae0be51
rjn <rjn@d90a992a-4ffb-0310-9b0e-ce574b03d632>
2008-06-04 10:28:01 +0000

    Latest version of SurfHk with four pidGoals

--- eventReaderRoot/trunk/SurfHk.h
+++ eventReaderRoot/trunk/SurfHk.h
@@ -12,6 +12,8 @@
 //Includes
 #include <TObject.h>
 #include "AnitaConventions.h"
+#include "simpleStructs.h"
+
 
 class SurfHk: public TObject
 {
@@ -19,7 +21,7 @@ class SurfHk: public TObject
   SurfHk();		 
   ~SurfHk();
 
-
+  SurfHk(Int_t trun, Int_t trealTime, FullSurfHkStruct_t *surfPtr);
 
   SurfHk(Int_t           trun,
 	 UInt_t          trealTime,
@@ -27,7 +29,7 @@ class SurfHk: public TObject
 	 UInt_t          tpayloadTimeUs,
 	 UShort_t        tglobalThreshold,
 	 UShort_t        terrorFlag,
-	 UShort_t        tscalerGoal,
+	 UShort_t        tscalerGoals[BANDS_PER_ANT],
 	 UShort_t        tupperWords[ACTIVE_SURFS],
 	 UShort_t        tscaler[ACTIVE_SURFS][SCALERS_PER_SURF],
 	 UShort_t        tthreshold[ACTIVE_SURFS][SCALERS_PER_SURF],
@@ -42,7 +44,7 @@ class SurfHk: public TObject
    UInt_t          payloadTimeUs;
    UShort_t        globalThreshold;
    UShort_t        errorFlag;
-   UShort_t        scalerGoal;
+   UShort_t        scalerGoals[BANDS_PER_ANT];
    UShort_t        upperWords[ACTIVE_SURFS];
    UShort_t        scaler[ACTIVE_SURFS][SCALERS_PER_SURF];
    UShort_t        threshold[ACTIVE_SURFS][SCALERS_PER_SURF];
@@ -60,7 +62,7 @@ class SurfHk: public TObject
    Int_t isBandMasked(int phi, AnitaRing::AnitaRing_t ring, AnitaBand::AnitaBand_t band);
    Int_t getLogicalIndex(int phi, AnitaRing::AnitaRing_t ring, AnitaBand::AnitaBand_t band);
 
-  ClassDef(SurfHk,10);
+  ClassDef(SurfHk,13);
 };
 
 
