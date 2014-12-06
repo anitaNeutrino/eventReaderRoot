1fde11d5d2fc35e9ad64d9e66d48a67691c8556e
rjn <rjn@d90a992a-4ffb-0310-9b0e-ce574b03d632>
2014-12-06 09:44:42 +0000

    Updating external temp sensor names

--- eventReaderRoot/trunk/CalibratedHk.cxx
+++ eventReaderRoot/trunk/CalibratedHk.cxx
@@ -286,7 +286,7 @@ Float_t CalibratedHk::getAttitude(int index) {
  const char *CalibratedHk::getExternalTempName(int index)
 {
 
-  const char *extTempNames[NUM_EXT_TEMPS]={"PV 1 (phi 4)","PV 3 (phi 8)","A01V (RFCM02)","A10V (RFCM07)","A03V (RFCM08)","A12V (RFCM12)","A05V (RFCM13)","A14V (RFCM14)","A07V (RFCM15)","A16V (RFCM16)","A17V (RFCM10)","A19V (RFCM03)","A21V (RFCM05)","A23V (RFCM06)","A25V (RFCM09)"};//,"A27V (RFCM11)","A29V (RFCM04)","A31V (RFCM01)","A??V (RFCM 17)","A??V (RFCM18)","A??V (RFCM19)","A??V (RFCM20)","Radiator Plate","PV 5 (phi 12)","PV 7 (phi 16)"};
+  const char *extTempNames[NUM_EXT_TEMPS]={"Radiator Plate, top right","Radiator Plate, bottom left","AMPA 16TH","Battery Box","AMPA 02TV","AMPA 02TV","ANITA box near He Drives","ANITA box near IRFMCs","PV17","Radiator Plate, middle","PV02","PV14","PV05","PV23","PV11"};//,"A27V (RFCM11)","A29V (RFCM04)","A31V (RFCM01)","A??V (RFCM 17)","A??V (RFCM18)","A??V (RFCM19)","A??V (RFCM20)","Radiator Plate","PV 5 (phi 12)","PV 7 (phi 16)"};
  if(index>=0 && index<NUM_EXT_TEMPS) 
     return extTempNames[index];
   return "None";
