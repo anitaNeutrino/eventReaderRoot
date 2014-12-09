98cf7ee09731df46b746b25644a3cda9e8770530
rjn <rjn@d90a992a-4ffb-0310-9b0e-ce574b03d632>
2014-12-09 00:21:26 +0000

    Updated SlowRate to current standard

--- eventReaderRoot/trunk/SlowRate.h
+++ eventReaderRoot/trunk/SlowRate.h
@@ -35,16 +35,17 @@ class SlowRate: public TObject
    Float_t longitude; ///< Payload longitude
    Short_t altitude; ///< Payload altitude
    UInt_t eventNumber; ///< Latest event number
-   UChar_t rfPwrAvg[ACTIVE_SURFS][RFCHAN_PER_SURF]; ///< The average rf power (in ADCs)
-   UChar_t avgScalerRates[TRIGGER_SURFS][SCALERS_PER_SURF]; 
-   UChar_t eventRate1Min; ///<Multiplied by 8
-   UChar_t eventRate10Min; ///<Multiplied by 8
+   UChar_t rfPwrAvg[ACTIVE_SURFS][RFCHAN_PER_SURF]; ///< The average rf power (in ADCs) //128
+   UChar_t avgScalerRates[TRIGGER_SURFS][SCALERS_PER_SURF]; ///<Divided by 4
+   UChar_t eventRate1Min; ///<Multiplied by 2
+   UChar_t eventRate10Min; ///<Multiplied by 2
 
    UChar_t temps[4];  ///<{SBS,SURF,TURF,RAD}
-   UChar_t powers[4]; ///<{PV V, +24V, BAT I, 24 I}
+   UChar_t powers[4]; ///<{PV V, +24V, PV I, 24 I}
 
    Double_t getRFPowerInK(int surf, int chan); ///< Returns the pseudo-calibrated RF power in K.
    Double_t getMeasuredRFPowerInK(int surf, int chan); ///< Returns the pseudo-calibrated RF power in K.
+   Double_t getRawRFPower(int surf, int chan);
    Int_t getAvgScaler(int surf, int ant); ///< Returns the averaged scaler value
   
    Float_t getLatitude() {return latitude;} ///< Returns the latitude
