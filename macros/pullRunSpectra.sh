2255fe56dd31a9e0e70722615d5327e0ddebfdab
LukeBatten <luke.batten.15@ucl.ac.uk>
2016-12-21 13:54:29 +1300

    Changing file type to match others

--- macros/pullRunSpectra.sh
+++ macros/pullRunSpectra.sh
@@ -5,10 +5,10 @@ echo Fetching run $runNumber...
 
 awareIP="157.132.95.239" # IP here, subject to change
 
-scp anita@$awareIP:/data/flight1617/telem/root/run$runNumber/gpuFile$runNumber.root . # Secure copy from anita-aware to current dir
+scp anita@$awareIP:/data/flight1617/telem/root/run$runNumber/gpuFile$runNumber.root . # Secure copy from anita-aware to current dir (/ change to your dir)
 
 root -b -l <<EOF # Start a root session
-.L quickPlotSpectra.cxx
+.L quickPlotSpectra.C
 quickPlotSpectra("gpuFile$runNumber.root",false,20)
 EOF
 # Save file as gpuFile gpuFile$runNumber.root
--- macros/quickPlotSpectra.C
+++ macros/quickPlotSpectra.C
@@ -1,7 +1,8 @@
-#include "GpuPowerSpectra.h" 
+#include "GpuPowerSpectra.h"
+
 void quickPlotSpectra(const char * gpuFile, bool backwards = false, int nplot = 20)
 { 
-
+  
   TFile f(gpuFile); 
 
   TTree * gpuTree = (TTree*) f.Get("gpuTree"); 
@@ -9,31 +10,39 @@ void quickPlotSpectra(const char * gpuFile, bool backwards = false, int nplot = 
   GpuPowerSpectra * gpu = 0; 
   gpuTree->SetBranchAddress("gpu",&gpu); 
 
-
-  TCanvas * c = new TCanvas; 
-
+  TCanvas * c = new TCanvas("c","c",1920,1080); 
   c->Divide(4,4); 
-
 
   if (nplot < 0 || nplot > gpuTree->GetEntries()) nplot = gpuTree->GetEntries();
   for (int i = 0; i < nplot; i++) 
-  {
-
-    gpuTree->GetEntry(backwards ? gpuTree->GetEntries() - i - 1 : i); 
-
-    for (int j = 0; j < 16; j++) 
     {
 
-      c->cd(j+1); 
-      TGraph * g = gpu->getGraph(j); 
-      g->SetLineColor(i+1); 
-      g->SetMarkerColor(i+1); 
+      gpuTree->GetEntry(backwards ? gpuTree->GetEntries() - i - 1 : i); 
 
-      g->SetTitle(TString::Format("Phi Sector %d",j+1)); 
-      g->Draw(i == 0 ? "alp" : "lpsame"); 
-      g->GetYaxis()->SetRangeUser(5,35); 
+      for (int j = 0; j < 16; j++) 
+	{
+	  c->cd(j+1);
+      
+	  TGraph * g = gpu->getGraph(j);
+            
+	  g->SetLineColor(3*i+3);
+	  g->SetMarkerColor(3*i+3);
+	  
+	  g->SetTitle(TString::Format("Phi Sector %d",j+1));
+	  g->Draw(i == 0 ? "alp" : "lpsame");
+	  g->GetYaxis()->SetLabelSize(0.06);
+	  g->GetYaxis()->SetRangeUser(5,35);
+	  //g->GetXaxis()->SetTitle("Frequency (MHz)");
+	  g->GetXaxis()->SetTitleSize(0.045);
+	  g->GetXaxis()->SetLabelOffset(0.01);
+	  g->GetXaxis()->SetTitleOffset(1.1);
+	  g->GetXaxis()->SetLabelSize(0.06);
+      
+	}
+
     }
 
-  }
-
+  TString pngFile = TString::Format("%s_Spec.png",gpuFile);
+  c->Print(pngFile,"png");
+  
 }
