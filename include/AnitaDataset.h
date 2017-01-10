8e92479ff23ec440c89ebc5bb42893fc3d680ae8
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2017-01-10 11:45:27 -0600

    add support for cuts and ordering by event in AnitaDataset as well as turf and surf loading

--- include/AnitaDataset.h
+++ include/AnitaDataset.h
@@ -12,6 +12,7 @@
  *  -it prefers timedHeader files, but can fall back to reading header files 
  *  -it also supports reading decimated files, but also requires the full header file (to get the correct entry in the other trees). 
  *
+ * 
  *
  *
  *  Cosmin Deaconu <cozzyd@kicp.uchicago.edu> 
@@ -20,6 +21,7 @@
 
 #include <vector> 
 #include "AnitaConventions.h" 
+
 class RawAnitaHeader; 
 class TTree; 
 class Adu5Pat; 
@@ -28,6 +30,10 @@ class RawAnitaEvent;
 class CalibratedAnitaEvent; 
 class PrettyAnitaHk; 
 class TFile;
+class TurfRate; 
+class TCut; 
+class TEventList; 
+class SurfHk; 
 
 
 class AnitaDataset
@@ -67,8 +73,51 @@ class AnitaDataset
     /** loads the first entry */ 
     int first() { return getEntry(0); } 
 
+    /** loads the last entry */
+    int last() { return getEntry(N()-1); }
+
     /** returns the number of entries */
     int N() const; 
+
+
+    /** Loads the firstEvent (in case the entries are not in order). returns the entry of it */ 
+    int firstEvent(); 
+
+    /** Loads next event (in case the entries are not in order). returns the entry of it */ 
+    int nextEvent(); 
+
+    /** Loads previous event (in case the entries are not in order). returns the entry of it */ 
+    int previousEvent(); 
+
+    /** Loads last event (in case the entries are not in order). returns the entry of it. */ 
+    int lastEvent(); 
+
+    /** Returns the nth event (in case the entries are not in order). returns the entry of it. */ 
+    int nthEvent(int i); 
+
+
+    /** Applies a cut to the entire dataset. Supercedes any previous cut. 
+     * Once you apply a cut, you may use NInCut, firstInCut(), nextInCut(), previousInCut(), lastInCut()
+     * to iterate.  The cut applies to the headTree. Returns the number of event sin the cut */
+    int setCut(const TCut & cut); 
+
+    /** The number of events in the cut (or -1 if no cut is applied) */
+    int NInCut() const; 
+
+    /** Loads the first event passing the cut. Returns the entry number or -1 if no cut applied */
+    int firstInCut(); 
+
+    /** Loads the last event passing the cut. Returns the entry number or -1 if no cut applied */
+    int lastInCut(); 
+
+    /** Loads the next passing the cut. Returns the entry number or -1 if no cut applied */
+    int nextInCut(); 
+
+   /** Loads the previous passing the cut. Returns the entry number or -1 if no cut applied */
+    int previousInCut(); 
+
+    /** Loads the nth event passing the cut. Returns the entry number or -1 if no cut applied */
+    int nthInCut(int i); 
 
 
     /** Loads the useful event. If force_reload is true, 
@@ -102,31 +151,52 @@ class AnitaDataset
     RawAnitaHeader * header(bool force_reload = false);
 
 
+    /** Loads the TURF Rate. This is indexed so that the fTurf->realTime is as close as possible to the header triggerTime */ 
+    TurfRate * turf(bool force_reload = false); 
+
+    /** Loads the SURF Hk. This is indexed so that the the payload times match as closely as possible */ 
+    SurfHk * surf(bool force_reload = false); 
+
+
+
+
   private: 
     void unloadRun(); 
     TTree * fHeadTree; 
     TTree * fDecimatedHeadTree; //only used when using decimated 
+    Int_t * fIndices; 
+    Int_t fIndex; 
     RawAnitaHeader * fHeader;
     TTree *fEventTree; 
     CalibratedAnitaEvent * fCalEvent; 
     RawAnitaEvent * fRawEvent; 
     UsefulAnitaEvent * fUseful; 
-    bool fUsefulDirty; 
-    bool fCalDirty;  // used only with raw data
-    bool fGpsDirty;  // used only with gpsFile data 
+    Bool_t fUsefulDirty; 
+    Bool_t fCalDirty;  // used only with raw data
+    Bool_t fGpsDirty;  // used only with gpsFile data 
     TTree* fGpsTree; 
     Adu5Pat * fGps; 
     TTree* fHkTree; 
     PrettyAnitaHk * fHk; 
 
+    TTree * fTurfTree; 
+    TurfRate * fTurf; 
+    Bool_t fTurfDirty; 
 
-    int fWantedEntry; 
-    int fDecimatedEntry; 
-    bool fHaveGpsEvent; 
-    bool fHaveCalibFile; 
+    TTree * fSurfTree; 
+    SurfHk * fSurf; 
+    Bool_t fSurfDirty; 
+
+
+    Int_t fWantedEntry; 
+    Int_t fDecimatedEntry; 
+    Bool_t fHaveGpsEvent; 
+    Bool_t fHaveCalibFile; 
     WaveCalType::WaveCalType_t fCalType; 
     std::vector<TFile *> filesToClose; 
     bool fDecimated; 
+    TEventList * fCutList; 
+    int fCutIndex; 
 }; 
 
 #endif 
--- src/AnitaDataset.cxx
+++ src/AnitaDataset.cxx
@@ -5,10 +5,16 @@
 #include "PrettyAnitaHk.h"
 #include "CalibratedAnitaEvent.h"
 #include "AnitaVersion.h" 
+#include <math.h>
 #include "TFile.h" 
 #include "TTree.h" 
 #include <stdlib.h>
 #include <unistd.h>
+#include "TurfRate.h" 
+#include "TMath.h"
+#include "SurfHk.h"
+#include "TEventList.h" 
+#include "TCut.h" 
 
 static bool checkIfFileExists(const char * file)
 {
@@ -74,7 +80,10 @@ AnitaDataset::AnitaDataset(int run, bool decimated, WaveCalType::WaveCalType_t c
   fHeadTree(0), fHeader(0), 
   fEventTree(0), fCalEvent(0), fRawEvent(0), fUseful(0), 
   fGpsTree(0), fGps(0), 
-  fHkTree(0), fHk(0) 
+  fHkTree(0), fHk(0),
+  fTurfTree(0), fTurf(0),
+  fSurfTree(0), fSurf(0),
+  fCutList(0)
 {
   setCalType(cal); 
   loadRun(run, decimated, version); 
@@ -94,6 +103,15 @@ void  AnitaDataset::unloadRun()
   fEventTree = 0; 
   fHkTree = 0; 
   fGpsTree = 0; 
+  fTurfTree = 0; 
+  fSurfTree = 0; 
+
+  if (fCutList) 
+  {
+    delete fCutList; 
+    fCutList = 0; 
+  }
+
 }
 
 
@@ -112,7 +130,7 @@ Adu5Pat * AnitaDataset::gps(bool force_load)
     if (fGpsDirty || force_load)
     {
       //try one that matches realtime
-      int gpsEntry = fGpsTree->GetEntryNumberWithBestIndex(header()->realTime); 
+      int gpsEntry = fGpsTree->GetEntryNumberWithBestIndex(round(header()->triggerTime + header()->triggerTimeNs  / 1e9)); 
       int offset = 0; 
       fGpsTree->GetEntry(gpsEntry); 
       while (fGps->attFlag == 1 && abs(offset) < 30)
@@ -242,6 +260,11 @@ UsefulAnitaEvent * AnitaDataset::useful(bool force_load)
 int AnitaDataset::getEntry(int entryNumber)
 {
 
+  //invalidate the indices 
+  fIndex = -1; 
+  fCutIndex=-1; 
+  
+ 
   if (entryNumber < 0 || entryNumber >= (fDecimated ? fDecimatedHeadTree : fHeadTree)->GetEntries())
   {
     fprintf(stderr,"Requested entry %d too big or small!\n", entryNumber); 
@@ -258,6 +281,8 @@ int AnitaDataset::getEntry(int entryNumber)
     fUsefulDirty = true; 
     fCalDirty = true; 
     fGpsDirty = true; 
+    fTurfDirty = true; 
+    fSurfDirty = true; 
   }
 
 
@@ -314,6 +339,17 @@ AnitaDataset::~AnitaDataset()
 
   if (fHk) 
     delete fHk; 
+
+  if (fTurf) 
+    delete fTurf; 
+
+  if (fSurf) 
+    delete fSurf; 
+
+
+  if (fCutList) 
+    delete fCutList; 
+
 }
 
 bool  AnitaDataset::loadRun(int run, bool dec,  int version) 
@@ -328,7 +364,7 @@ bool  AnitaDataset::loadRun(int run, bool dec,  int version)
 
 
   //seems like a good idea 
-  if (version) AnitaVersion::set(version); 
+  if (version>0) AnitaVersion::set(version); 
 
   //if decimated, try to load decimated tree
 
@@ -344,6 +380,7 @@ bool  AnitaDataset::loadRun(int run, bool dec,  int version)
       fDecimatedHeadTree = (TTree*) f->Get("headTree"); 
       fDecimatedHeadTree->BuildIndex("eventNumber"); 
       fDecimatedHeadTree->SetBranchAddress("header",&fHeader); 
+      fIndices = fDecimatedHeadTree->GetIndex(); 
     }
     else
     {
@@ -376,7 +413,10 @@ bool  AnitaDataset::loadRun(int run, bool dec,  int version)
   }
 
   if (!fDecimated) fHeadTree->SetBranchAddress("header",&fHeader); 
+
   fHeadTree->BuildIndex("eventNumber"); 
+
+  if (!fDecimated) fIndices = fHeadTree->GetIndex(); 
 
   //try to load gps event file  
   TString fname = TString::Format("%s/run%d/gpsEvent%d.root", data_dir, run, run); 
@@ -439,14 +479,14 @@ bool  AnitaDataset::loadRun(int run, bool dec,  int version)
     {
       if (checkIfFileExists(fname3.Data()))
       {
-	TFile * f = new TFile(fname3.Data()); 
-	filesToClose.push_back(f); 
-	fEventTree = (TTree*) f->Get("eventTree"); 
-	fHaveCalibFile = false;
-	fEventTree->SetBranchAddress("event",&fUseful);
+        TFile * f = new TFile(fname3.Data()); 
+        filesToClose.push_back(f); 
+        fEventTree = (TTree*) f->Get("eventTree"); 
+        fHaveCalibFile = false;
+        fEventTree->SetBranchAddress("event",&fUseful);
       } else {
-	fprintf(stderr,"Could not find event file for run %d, giving up!\n",run); 
-	return false;
+        fprintf(stderr,"Could not find event file for run %d, giving up!\n",run); 
+        return false;
       }
     } 
   }
@@ -458,19 +498,218 @@ bool  AnitaDataset::loadRun(int run, bool dec,  int version)
      TFile * f = new TFile(fname.Data()); 
      filesToClose.push_back(f); 
      fHkTree = (TTree*) f->Get("prettyHkTree"); 
+     fHkTree->SetBranchAddress("hk",&fHk); 
   }
   else 
   {
-    fprintf(stderr,"Could not find hk file for run %d, giving up!\n",run); 
-    return false; 
+    fHkTree = 0; 
+    fprintf(stderr,"Could not find hk file for run %d, no HK will be available!\n",run); 
   }
 
-  fHkTree->SetBranchAddress("hk",&fHk); 
+
+
+  // try to load turf file 
+  fname = TString::TString::Format("%s/run%d/turfRateFile%d.root",data_dir,run,run); 
+  if (checkIfFileExists(fname.Data()))
+  {
+
+     TFile * f = new TFile(fname.Data()); 
+     filesToClose.push_back(f); 
+     fTurfTree = (TTree*) f->Get("turfRateTree"); 
+     fTurfTree->BuildIndex("realTime"); 
+     fTurfTree->SetBranchAddress("turf",&fTurf); 
+  }
+  else 
+  {
+    fTurfTree = 0; 
+    fprintf(stderr,"Could not find TurfRate file for run %d, no Turf will be available!\n",run); 
+  }
+
+  // try to load tusf file 
+  fname = TString::TString::Format("%s/run%d/SurfHkFile%d.root",data_dir,run,run); 
+  if (checkIfFileExists(fname.Data()))
+  {
+
+     TFile * f = new TFile(fname.Data()); 
+     filesToClose.push_back(f); 
+     fSurfTree = (TTree*) f->Get("surfHkTree"); 
+     fSurfTree->BuildIndex("payloadTime","payloadTimeUs"); 
+     fSurfTree->SetBranchAddress("surf",&fSurf); 
+  }
+  else 
+  {
+    fSurfTree = 0; 
+    fprintf(stderr,"Could not find SurfHK file for run %d, no SURF will be available!\n",run); 
+  }
+
+
+  //load the first entry 
   getEntry(0); 
+  
+
   return true; 
+}
+
+
+int AnitaDataset::previousEvent() 
+{
+  if (fIndex < 0) 
+  {
+    fIndex = TMath::BinarySearch(N(), fIndices, fDecimated ? fDecimatedEntry : fWantedEntry); 
+  }
+
+  if (fIndex >0) 
+    fIndex--; 
+
+  return nthEvent(fIndex); 
+}
+
+
+int AnitaDataset::firstEvent()
+{
+  return nthEvent(0); 
+}
+
+int AnitaDataset::lastEvent()
+{
+  return nthEvent(N()-1); 
+}
+
+int AnitaDataset::nthEvent(int n)
+{
+  int ret = getEntry(fIndices[n]); 
+  fIndex = n; 
+  return ret; 
+}
+
+
+
+int AnitaDataset::nextEvent() 
+{
+  if (fIndex < 0) 
+  {
+    fIndex = TMath::BinarySearch(N(), fIndices, fDecimated ? fDecimatedEntry : fWantedEntry); 
+  }
+
+  if (fIndex < N() -1)
+    fIndex++; 
+
+  return nthEvent(fIndex); 
 }
 
 int AnitaDataset::N() const
 {
   return (fDecimated? fDecimatedHeadTree : fHeadTree)->GetEntries(); 
 }
+
+
+
+int AnitaDataset::setCut(const TCut & cut)
+{
+  if (fCutList) 
+  {
+    delete fCutList; 
+  }
+
+  int n = (fDecimated? fDecimatedHeadTree : fHeadTree)->Draw(">>evlist1",cut,"goff"); 
+  fCutList = (TEventList*) gDirectory->Get("evlist1");
+  return n; 
+}
+
+
+
+int AnitaDataset::NInCut() const
+{
+
+  if (fCutList)
+  {
+    return fCutList->GetN(); 
+  }
+
+  return -1; 
+}
+
+int AnitaDataset::firstInCut() 
+{
+  return nthInCut(0); 
+}
+
+int AnitaDataset::lastInCut() 
+{
+  return nthInCut(NInCut()-1); 
+}
+
+
+int AnitaDataset::nthInCut(int i)
+{
+  if (!fCutList) 
+    return -1; 
+  int ret = getEntry(fCutList->GetEntry(i)); 
+
+  fCutIndex = i;
+  return ret;
+}
+
+int AnitaDataset::nextInCut()
+{
+  if (!fCutList) return -1; 
+  if (fCutIndex < 0) 
+  {
+    fCutIndex = TMath::BinarySearch(NInCut(), fCutList->GetList(), (Long64_t) (fDecimated ? fDecimatedEntry : fWantedEntry)); 
+  }
+
+  if (fCutIndex <  NInCut() - 1) 
+  {
+    fCutIndex++; 
+  }
+  return nthInCut(fCutIndex); 
+
+}
+
+int AnitaDataset::previousInCut()
+{
+  if (!fCutList) return -1; 
+  if (fCutIndex < 0) 
+  {
+    fCutIndex = TMath::BinarySearch(NInCut(), fCutList->GetList(),(Long64_t)  (fDecimated ? fDecimatedEntry : fWantedEntry)); 
+  }
+
+  if (fCutIndex >  0) 
+  {
+    fCutIndex--; 
+  }
+  return nthInCut(fCutIndex); 
+
+}
+
+
+TurfRate* AnitaDataset::turf(bool force_reload) 
+{
+
+  if (!fTurfTree) return 0; 
+
+  if (fTurfDirty || force_reload) 
+  {
+    int entry = fTurfTree->GetEntryNumberWithBestIndex(round(header()->triggerTime + header()->triggerTimeNs /1e9 ) ); 
+    fTurfTree->GetEntry(entry); 
+    fTurfDirty = false; 
+  }
+
+  return fTurf; 
+}
+
+SurfHk* AnitaDataset::surf(bool force_reload) 
+{
+
+  if (!fSurfTree) return 0; 
+
+  if (fSurfDirty || force_reload) 
+  {
+    int entry = fSurfTree->GetEntryNumberWithBestIndex(header()->payloadTime, header()->payloadTimeUs); 
+    fSurfTree->GetEntry(entry); 
+    fSurfDirty = false; 
+  }
+
+  return fSurf; 
+}
+
