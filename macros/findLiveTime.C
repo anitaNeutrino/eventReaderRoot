f87a8837b98734eeacad815ba7cec75736323cf1
LukeBatten <luke.batten.15@ucl.ac.uk>
2017-04-04 19:57:54 +0100

    Preliminary program to find live time / find missing events / times for flight log

---
+++ macros/findLiveTime.C
@@ -0,0 +1,167 @@
+// Quick macro for finding live time
+// As some Hk data is unavailable, rendering pat information unfindable, I'm using this to determine the live time instead of say, event data
+
+// Identifies missing realTime periods, and looks for suspicious gaps
+// Successfully finds runs w/ gps outages, missing events etc
+
+#include "AnitaConventions.h"
+#include "UsefulAnitaEvent.h"
+#include "AnitaGeomTool.h"
+#include "RawAnitaHeader.h"
+#include "TimedAnitaHeader.h"
+#include "PrettyAnitaHk.h"
+#include "FFTtools.h"
+#include "TurfRate.h"
+#include "Adu5Pat.h"
+#include "TFile.h"
+#include "TTree.h"
+#include "TChain.h"
+#include "TGraph.h"
+#include "TCanvas.h"
+#include "TLegend.h"
+#include "TH2.h"
+#include "TH1.h"
+#include "TColor.h"
+#include "TImage.h"
+#include "TMath.h"
+#include "TStyle.h"
+#include "TSystem.h"
+#include <iostream>
+#include <fstream>
+#include "TMarker.h"
+#include "TLatex.h"
+#include "TEllipse.h"
+#include "TLine.h"
+#include "TPaveText.h"
+
+void findLiveTime(int firstRun, int lastRun);
+
+void findLiveTime(int firstRun, int lastRun)
+{
+
+  FileStat_t staty;
+  char prettyName[FILENAME_MAX];
+  TChain *prettyChain = new TChain("prettyHkTree");
+  
+  for (int run=firstRun; run<lastRun+1; run++)
+    { 
+      sprintf(prettyName,"/unix/anita4/flight2016/root/run%d/prettyHkFile%d.root",run,run);
+      if(gSystem->GetPathInfo(prettyName,staty))
+	{
+	  continue;
+	}
+      prettyChain->Add(prettyName);
+    }
+  
+  PrettyAnitaHk *hk = 0;
+  prettyChain->SetBranchAddress("hk",&hk); 
+  int maxEntries = prettyChain->GetEntries();
+  cout << "There are " << maxEntries << " entries in prettyHk Chain " << endl;
+
+  UInt_t startTime = 0;
+  UInt_t finalTime = 0;
+  UInt_t realTimeC = 0; // current realTime entry
+  UInt_t realTimeL = 0; // last realTime entry
+
+  UInt_t typicalMaxRunTransitionTime = 21; // realTime difference between runs
+  UInt_t currentSuspiciousRun = firstRun;
+  UInt_t lastSuspiciousRun = firstRun;
+  UInt_t suspiciousRuns = 0;
+  
+  UInt_t missingTimePeriod = 0;
+  UInt_t totalTime = 0;
+  UInt_t totalTimeMissing = 0;
+  UInt_t liveTime = 0;
+  Double_t liveTimePerc = 0;
+
+  for (int entry = 0; entry < maxEntries; entry++) 
+   {
+     
+    prettyChain->GetEntry(entry);
+    
+    realTimeC = hk->realTime;
+
+    if(entry == 0)
+      {
+	realTimeL = hk->realTime;
+
+	startTime = hk->realTime;
+      }
+
+    else
+      {
+	prettyChain->GetEntry(entry-1);
+	realTimeL = hk->realTime;
+      }
+    
+    if( (realTimeC != (realTimeL + 1))  &&  (realTimeC != realTimeL) )
+      {
+
+	std::cout << "Run is: " << hk->run << std::endl;
+	
+	std::cout << "realTimeC = " << realTimeC << std::endl;
+	std::cout << "realTimeL = " << realTimeL << std::endl;
+
+	missingTimePeriod = 0;
+
+	missingTimePeriod = realTimeC - realTimeL;
+	
+	if( (realTimeC - realTimeL) > 4294967290) // bug where we get max int or similar
+	  {
+	    missingTimePeriod = 0;
+	  }
+
+	if(missingTimePeriod > typicalMaxRunTransitionTime * 2) // sometimes the run resets twice, find values higher than these
+	  {
+	    currentSuspiciousRun = hk->run;
+
+	    if(currentSuspiciousRun != lastSuspiciousRun)
+	      {
+		std::cout << "SUSPICIOUS RUN DETECTED: run " << hk->run << std::endl;
+		suspiciousRuns++;
+	      }
+	   
+	    lastSuspiciousRun = hk->run;
+	  }
+	
+	std::cout << "Transition in real time:" << missingTimePeriod << std::endl;
+	
+	totalTimeMissing += missingTimePeriod;
+
+      }
+
+    if(entry == maxEntries - 1)
+      {
+
+	std::cout << "|||||||||||||||| Total flight stats |||||||||||||||" << std::endl;
+	
+	finalTime = hk->realTime;
+
+	totalTime = finalTime - startTime;
+	
+	liveTime = totalTime - totalTimeMissing;
+
+	std::cout << "Total missing time = " << totalTimeMissing << " seconds" << std::endl;
+
+	std::cout << " = " << Double_t(totalTimeMissing)/Double_t(60*60) << " hours " << std::endl;
+	
+	std::cout << "Total live time = " << liveTime << " seconds" << std::endl;
+
+	std::cout << "= " << Double_t(liveTime)/(Double_t(60*60*24)) << " days " << std::endl;
+	
+	liveTimePerc = Double_t(totalTime - totalTimeMissing)/Double_t(totalTime) * 100;
+	std:: cout << "% live time = " << liveTimePerc << std::endl;
+
+	std::cout << "Total # suspicious runs = " <<  suspiciousRuns << std::endl;
+
+	std::cout << "||||||||||||||||                    |||||||||||||||" << std::endl;
+
+	
+	
+      }
+
+   } // end entry loop
+
+  return;
+  
+}
